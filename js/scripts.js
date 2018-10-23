a24d2bb5cf80eef8e48fc8787ae76c926bf98b51
Brittany Gollins and Michael Butcher <brittany@>
2018-10-22 20:22:26 -0700

    turnTotal finally keeps total of the turn

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
 //Business Logic
 function PlayerRoll(roll, turnTotal, finalScore) {
-  this.roll = 0
-  this.turnTotal = 0
-  this.finalScore = 0
+  this.roll = 0;
+  this.turnTotal = 0;
+  this.finalScore = 0;
 };
 
 PlayerRoll.prototype.randomizer = function() {
-  this.roll = Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+PlayerRoll.prototype.rollDie = function() {
+  //this.roll = Math.floor(Math.random() * 6) + 1;
   if (this.roll === 1) {
+    this.turnTotal = 0;
     alert("You're done");
   } else {
-  return this.roll;
-  }; 
-};
+    console.log("The latest roll: " + this.roll)
+    this.turnTotal += this.roll;
+  }
+  console.log("The Total for the turn: " + this.turnTotal);
+  };
+
 
 // UI logic
 $(document).ready(function() {
-  $("form.Player1").submit(function(event) {
-      event.preventDefault();
-    var Roll1 = $("#Roll1").val();
-    var turnTotal1 = 0;
-    var finalScore1 = 0;
 
-    var newPlayer1 = new PlayerRoll(Roll1, turnTotal1, finalScore1);
-    var output = newPlayer1.randomizer();
-    console.log(output);
+  var newPlayer1 = new PlayerRoll();
+
+  $("#Roll1").click(function(event) {
+      event.preventDefault();
+    // var Roll1 = $("#Roll1").val();
+    // var turnTotal1 = 0;
+    // var finalScore1 = 0;
+//    debugger;
+    newPlayer1.roll = newPlayer1.randomizer();
+    newPlayer1.rollDie();
+    // console.log(output);
+
   });
+
+  //hold click event
+
 });
