95e104352f75351290e838e002e9a35bbac587bb
Student <example@example.com>
2018-10-24 20:11:57 -0700

    Back end logic can recognize 100 points as a win

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,21 @@ PlayerRoll.prototype.rollDie = function() {
     $("#Hold1").hide();
     $("#Roll2").show();
     $("#Hold2").show();
+
   } else {
-    console.log("The latest roll: " + this.roll);
+    console.log("The latest roll: " + this.roll)
     this.turnTotal += this.roll;
+    if ((this.finalScore + this.turnTotal) < 100) {
+    console.log("The latest roll (WIN CHECK): " + this.roll)
+    console.log("Player 1 Overall score is: " + this.finalScore)
+    } else {
+      console.log("Your Overall score is: " + (this.finalScore + this.turnTotal) + " YOU WIN!");
+          $("#Roll2").hide();
+          $("#Hold2").hide();
+    }
   }
   console.log("The Total for the turn: " + this.turnTotal);
-  };
+};
 
 PlayerRoll.prototype.holdEm = function() {
   this.finalScore += this.turnTotal;
@@ -28,8 +37,9 @@ PlayerRoll.prototype.holdEm = function() {
   $("#Hold1").hide();
   $("#Roll2").show();
   $("#Hold2").show();
+  this.roll = 0;
   this.turnTotal = 0;
-  console.log("Your Overall score is: " + this.finalScore);
+
 };
 
 
@@ -53,6 +63,14 @@ PlayerRoll2.prototype.rollDie2 = function() {
   } else {
     console.log("The latest roll: " + this.roll2)
     this.turnTotal2 += this.roll2;
+    if ((this.finalScore2 + this.turnTotal2) < 100) {
+    console.log("The latest roll: " + this.roll2)
+    console.log("Player 2 Overall score is: " + this.finalScore2)
+    } else {
+      console.log("Your Overall score is: " + (this.finalScore2 + this.turnTotal2) + " YOU WIN!");
+          $("#Roll1").hide();
+          $("#Hold1").hide();
+    }
   }
   console.log("The Total for the turn: " + this.turnTotal2);
   };
@@ -65,7 +83,7 @@ PlayerRoll2.prototype.holdEm2 = function() {
   $("#Roll1").show();
   $("#Hold1").show();
   this.turnTotal2 = 0;
-  console.log("Your Overall score is: " + this.finalScore2);
+  this.roll2 = 0;
 };
 
 // UI logic
